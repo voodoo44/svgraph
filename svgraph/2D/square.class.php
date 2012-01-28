01f3b47232973905d60135228c6edb8cb192b22c
Gunter Thomas <gunter@gunter-thomas.de>
2012-01-28 12:34:54 +0100

    -- 2nd commit

---
+++ svgraph/2D/square.class.php
@@ -0,0 +1,110 @@
+<?php
+
+/**
+ * Class creates a Square-Object and returns it as SVG
+ * @author Gunter Thomas
+ */
+Class Square {
+    /**
+     * The Gridobject itself
+     * @var String
+     */
+    protected $_grid = null;
+
+    /**
+     * The Gridelements
+     * @var Array
+     */
+    protected $_aElements = array();
+
+    /**
+     * The size of the Chart
+     * @var Array
+     */
+    protected $_size = array();
+
+    /**
+     * Initializes the Square-Chart Object with size 500x500px
+     */
+    public function __construct() {
+        $this->_size['width'] = "500";
+        $this->_size['height'] = "500";
+
+        $this->_grid = new SimpleXMLElement('<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.0//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="500px" height="500px"></svg>');
+    }
+
+    /**
+     * Sets the size of the Chart
+     * @param String $width
+     * @param String $height
+     */
+    public function setSize($width="800", $height="600") {
+        $this->_size['width'] = $width;
+        $this->_size['height'] = $height;
+        $this->_grid = null;
+        $this->_grid = new SimpleXMLElement(
+                           '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.0//EN"
+                           "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
+                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                            width="'.$width.'" height="'.$height.'"></svg>'
+                        );
+    }
+
+    /**
+     * Adds an Element to the Chart
+     * @param Array $aElement
+     */
+    public function addElement($aElement = array()) {
+        $aElement['cy'] = $this->_size['height'] - $aElement['cy'];
+        $this->_aElements[] = $aElement;
+    }
+
+    /**
+     * Initiates a Gridobject and sets the Grid
+     * @param Array $aGrid
+     */
+    public function setGrid($aGrid = array()) {
+        $oXml = $this->_grid;
+        $oXmlGrid = $oXml->addChild('g');
+        $oXmlGrid->addAttribute('id', 'grid');
+        $oXmlGrid->addAttribute('stroke', 'black');
+
+        // x-Line
+        $oXmlGridDetails = $oXmlGrid->addChild('line');
+        $oXmlGridDetails->addAttribute('x1', '0');
+        $oXmlGridDetails->addAttribute('y1', '0');
+        $oXmlGridDetails->addAttribute('x2', $this->_size['width']);
+        $oXmlGridDetails->addAttribute('y2', '0');
+        $oXmlGridDetails->addAttribute('style', 'stroke-dasharray:1,' . $aGrid['x']['quadSize'] . ';stroke-width:' . $this->_size['height']*40 . ';');
+
+        // y-Line
+        $oXmlGridDetails = $oXmlGrid->addChild('line');
+        $oXmlGridDetails->addAttribute('x1', '0');
+        $oXmlGridDetails->addAttribute('y1', $this->_size['height']);
+        $oXmlGridDetails->addAttribute('x2', '0');
+        $oXmlGridDetails->addAttribute('y2', '0');
+        $oXmlGridDetails->addAttribute('style', 'stroke-dasharray:1,' . $aGrid['y']['quadSize'] . ';stroke-width:' . $this->_size['width']*40 . ';');
+    }
+
+    /**
+     * Returns the SVG-Code of the created Chart
+     * @return SVG-Chart as a String
+     */
+    public function getSVG() {
+        $oXmlGrid = $this->_grid->g;
+        $aElements = $this->_aElements;
+
+        foreach($aElements as $aElement) {
+            $oXmlGridChild = $oXmlGrid->addChild($aElement['type']);
+            $oXmlGridChild->addAttribute('cx', $aElement['cx']);
+            $oXmlGridChild->addAttribute('cy', $aElement['cy']);
+            $oXmlGridChild->addAttribute('r', $aElement['r']);
+            $oXmlGridChild->addAttribute('stroke', $aElement['stroke']);
+            $oXmlGridChild->addAttribute('stroke-width', $aElement['stroke-width']);
+            $oXmlGridChild->addAttribute('fill', $aElement['fill']);
+        }
+
+        return $this->_grid->asXml();
+    }
+}
